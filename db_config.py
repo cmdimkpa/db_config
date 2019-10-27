20799fef64b74d7840c7a6be9083090c1071c47c
Monty Dimkpa <cmdimkpa@gmail.com>
2019-10-27 18:11:27 +0100

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -91,8 +91,8 @@ elif mode == "import_config":
     try:
         url = sys.argv[2]
         try:
-            config = http.get(url).json()
-            print(write_config(str(config)))
+            config = eval(http.get(url).content)
+            print(write_config(config))
         except:
             print("Config file Not JSON Serializable, exiting...")
             sys.exit()
